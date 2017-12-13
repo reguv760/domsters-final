a95d1af0882477f7fd01caf69885cd7b7945b0de
kindluddite <howley.phil@gmail.com>
2017-12-13 15:36:39 -0800

    add new github build file

--- gulp/tasks/github-build.js
+++ gulp/tasks/github-build.js
@@ -1,57 +1,91 @@
 const gulp = require('gulp'),
-imagemin = require('gulp-imagemin'),
-del = require('del'),
-usemin = require('gulp-usemin'),
-rev = require('gulp-rev'),
-cssnano = require('gulp-cssnano'),
-uglify = require('gulp-uglify'),
-browserSync = require('browser-sync').create();
+  imagemin = require('gulp-imagemin'),
+  del = require('del'),
+  usemin = require('gulp-usemin'),
+  rev = require('gulp-rev'),
+  cssnano = require('gulp-cssnano'),
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync').create();
 
-gulp.task('previewDist', function() {
+gulp.task('previewDocs', function() {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: './docs'
-    }
+      baseDir: './docs',
+    },
   });
 });
 
-gulp.task('deleteDistFolder', function() {
+gulp.task('deleteDocsFolder', ['icons'], function() {
   return del('./docs');
 });
 
-gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+gulp.task('copyGeneralFiles', ['deleteDocsFolder'], function() {
   var pathsToCopy = [
     './src/**/*',
     '!./src/index.html',
     '!./src/assets/images/**',
     '!./src/assets/styles/**',
     '!./src/assets/scripts/**',
-    '!./src/temp/*',
-    '!./src/temp/**'
-  ]
+    '!./src/temp/',
+    '!./src/temp/**',
+  ];
 
-  return gulp.src(pathsToCopy)
-    .pipe(gulp.dest('./docs'));
+  return gulp.src(pathsToCopy).pipe(gulp.dest('./docs'));
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
-  return gulp.src(['./src/assets/images/**/*', '!./src/assets/images/icons', '!./src/assets/images/icons/**/*'])
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      multipass: true
-    }))
+gulp.task('optimizeImages', ['deleteDocsFolder'], function() {
+  return gulp
+    .src([
+      './src/assets/images/**/*',
+      '!./src/assets/images/icons',
+      '!./src/assets/images/icons/**/*',
+    ])
+    .pipe(
+      imagemin({
+        progressive: true,
+        interlaced: true,
+        multipass: true,
+      })
+    )
     .pipe(gulp.dest('./docs/assets/images'));
 });
 
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
-  return gulp.src('./src/index.html')
-    .pipe(usemin({
-      css: [function() {return rev()}, function() {return cssnano()}],
-      js: [function() {return rev()}, function() {return uglify()}],
-    }))
+gulp.task('useminTrigger', ['deleteDocsFolder'], function() {
+  gulp.start('usemin');
+});
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
+  return gulp
+    .src('./src/index.html')
+    .pipe(
+      usemin({
+        css: [
+          function() {
+            return rev();
+          },
+          function() {
+            return cssnano();
+          },
+        ],
+        js: [
+          function() {
+            return rev();
+          },
+          function() {
+            return uglify();
+          },
+        ],
+      })
+    )
     .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('github-build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
+gulp.task('github-build', [
+  'deleteDocsFolder',
+  'copyGeneralFiles',
+  'createSprite',
+  'copySpriteGraphic',
+  'optimizeImages',
+  'useminTrigger',
+]);
